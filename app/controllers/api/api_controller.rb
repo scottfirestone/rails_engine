70414658b99342e8d92b9816b810ed62863e3b8a
Scott Firestone <scott.firestone.dev@gmail.com>
2016-04-12 14:09:18 -0600

    Move attr_mapper method from application controller to apicontroller

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,4 +1,18 @@
 module Api
   class ApiController < ApplicationController
+    def attr_mapper(params)
+      @model      = params[:model].singularize.classify.constantize
+      @attr_type  = params.first[0]
+      @attr_val   = set_attr_val(params)
+    end
+
+    private
+      def set_attr_val(params)
+        if ["id", "quantity", "unit_price", "item_id", "invoice_id", "customer_id", "merchant_id"].include?(@attr_type)
+          params.first[1].to_i
+        else
+          params.first[1]
+        end
+      end
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  def attr_mapper(params)
-    @model      = params[:model].singularize.classify.constantize
-    @attr_type  = params.first[0]
-    @attr_val   = set_attr_val(params)
-  end
-
-  private
-    def set_attr_val(params)
-      if ["id", "quantity", "unit_price", "item_id", "invoice_id", "customer_id", "merchant_id"].include?(@attr_type)
-        params.first[1].to_i
-      else
-        params.first[1]
-      end
-    end
 end
